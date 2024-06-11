30f765374d6ac5791a20087ee2aa2575c77f48a7
Fergago20 <fergomez@uamv.edu.ni>
2024-06-11 09:22:36 -0600

    first commit

---
+++ proyecto.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+char n_empresa[20], n_gerente[20], pro_ser[20];
+printf("Ingrese nombre de la empresa\n");
+scanf("%s", n_empresa);
+printf("\nNombre del gerente\n");
+scanf("%s", n_gerente);
+printf("\nProducto o servicios que ofrece\n");
+scanf("%s", pro_ser);
+    return 0;
+}
